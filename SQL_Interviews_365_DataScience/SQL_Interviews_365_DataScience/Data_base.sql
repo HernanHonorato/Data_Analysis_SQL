1c55c59b9454096cb8c18443bfdd5a8348e1c9db
HernanHonorato <hernanhonorato@gmail.com>
2024-05-22 09:40:03 -0300

    Update Data_base.sql

--- SQL_Interviews_365_DataScience/SQL_Interviews_365_DataScience/Data_base.sql
+++ SQL_Interviews_365_DataScience/SQL_Interviews_365_DataScience/Data_base.sql
@@ -1,4 +1,8 @@
- create schema interviews;
+Este repositorio contiene el código para crear la base de datos, y las preguntas sobre dicha base,
+realizadas durante el curso "SQL for Data Science Interviews" de Tina Huang. 
+
+
+create schema interviews;
 
   set schema 'interviews';
 
